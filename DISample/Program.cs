1886d574f5b048f54ee2f21a2e756d068e114de5
Shuhei Nishizawa <shuhey.dev@gmail.com>
2019-04-29 13:33:37 +0900

    プロジェクト ファイルを追加します。

---
+++ DISample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DISample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new A();
+        }
+    }
+
+
+
+    class A
+    {
+        public A()
+        {
+            //クラスBのインスタンスを使う。
+            var b = new B();
+
+            Console.WriteLine(b.CalcAdd(1, 2));
+        }
+    }
+
+    class B
+    {
+        public int CalcAdd(int x, int y)
+        {
+
+            return (x + y);
+        }
+    }
+}
